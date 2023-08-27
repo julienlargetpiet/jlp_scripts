cf9556ea0a0d629c94de4b81298e954e377338ce
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-08-27 13:17:56 +0200

    Update bright_down.sh

--- bright_down.sh
+++ bright_down.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#put your current brightness in the file ~/bright.txt
+
 a=$(cat ~/bright.txt)
 
 if [ "$a" = ".05" ]
@@ -11,9 +13,11 @@ else
 
         a=$(bc -l <<< $a-0.05)
 
-        xrandr --output DP-0 --brightness $a
+        xrandr --output YOUR_SCREEN --brightness $a
 
-        xrandr --output HDMI-0 --brightness $a
+        #xrandr --output YOUR_SECOND_SCREEN --brightness $a
+
+        #...
 
         echo $a > ~/bright.txt
 

102f4c7ed11da72e405934f02ab4b6ed11959883
Julien <pgyygp090@gmail.com>
2024-10-16 15:22:30 +0200

    fixed bug in except_programm.py

--- rm_except/except_programm.py
+++ rm_except/except_programm.py
@@ -14,14 +14,16 @@ if except_file[0] == "{":
         if i[0] == "*":
             except_file2 += glob(f"{cur_path}/{i}")
         else:
-            except_file2.append(i)
+            except_file2.append(cur_path + "/" + i)
     for i in glob(f"{cur_path}/*"):
         if i not in except_file2:
-            os.system(f"rm {cur_path}/{i}")
+            os.system(f"rm {i}")
 else:
     cur_path = "/".join(cur_path[0:(len(cur_path) - 1)])
     if except_file[0] == "*":
         except_file = glob(f"{cur_path}/{except_file}")
+    else:
+        except_file = cur_path + "/" + except_file
     for i in glob(f"{cur_path}/*"):
         if i not in except_file:
             os.system(f"rm {i}")
